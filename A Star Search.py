417e21d3a67b468519e75d1df85b4ae3a498c77e
awaisashraf00 <ashrafawais915@gmail.com>
2025-08-25 18:35:24 +0500

    THE PATH FINDER

--- A Star Search.py
+++ A Star Search.py
@@ -5,7 +5,7 @@ from queue import PriorityQueue
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("A* Path Finding Algorithm")
+pygame.display.set_caption("A* Path Finding Algorithm Implementation")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -89,7 +89,6 @@ def h(p1, p2):
 	x1, y1 = p1
 	x2, y2 = p2
 	return abs(x1 - x2) + abs(y1 - y2)
-
 
 def reconstruct_path(came_from, current, draw):
 	while current in came_from:
